aa1332a657cd613b07739ae28f32d69c82d7d017
Joshua <joshuaisverycool2024@gmail.com>
2026-09-21 19:31:02 +0100

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import WindowsWorkspace from "@/components/os/WindowsWorkspace";
-
-export const dynamic = "force-dynamic";
-
-export default function HomePage() {
-  return <WindowsWorkspace />;
-}
-
